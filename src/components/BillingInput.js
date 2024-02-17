a7b2a1bf9c77ecce27926c790f338b1c579130d9
youthinnco <hidiler9@gmail.com>
2024-02-18 01:40:47 +0300

    Update BillingInput.js

--- src/components/BillingInput.js
+++ src/components/BillingInput.js
@@ -14,10 +14,10 @@ const BillingInput = () => {
     const [excelFile, setExcelFile] = useState(null);
     const [items, setItems] = useState([]);
     const [productCode, setProductCode] = useState('');
-    const [ setProductName] = useState('');
+    const [productName, setProductName] = useState('');
     const [productCount, setProductCount] = useState('');
-    const [ setUnitPrice] = useState('');
-    const [ setTotalPrice] = useState('');
+    const [unitPrice, setUnitPrice] = useState('');
+    const [totalPrice, setTotalPrice] = useState('');
     const [customerType, setCustomerType] = useState('');
     const [filteredProductName, setFilteredProductName] = useState('');
     const [similarProducts, setSimilarProducts] = useState([]);
@@ -181,8 +181,6 @@ const BillingInput = () => {
                     setProductCode(code);
                     setProductName(name);
                     setUnitPrice(price);
-
-                    console.log('count:', productCount);
 
                     newItem = {
                         productCode: code,
